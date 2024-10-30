13c9b6641a178d4fd47bdbce4f93a3d6b3db7c82
ibrahim kedir <techofreact@gmail.com>
2024-10-30 15:42:40 +0300

    ft: trusted by section.

--- front-page.php
+++ front-page.php
@@ -171,6 +171,26 @@ get_header(); ?>
                 </div>
             </div>
         </section>
+        <!-- trusted by section -->
+        <section class="trusted_by_section">
+            <div class="trusted_header_title">
+                <h2>Trusted By 50+ Companies</h2>
+            </div>
+            <div class="gained_trusts">
+                <div class="company_gained_wrapper">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/amhara_bank.png" alt="amhara_bank_icon" />
+                </div>
+                <div class="company_gained_wrapper">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/giz.png" alt="giz" />
+                </div>
+                <div class="company_gained_wrapper">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/telecom.png" alt="telecom" />
+                </div>
+                <div class="company_gained_wrapper">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cbe.png" alt="cbe" />
+                </div>
+            </div>
+        </section>
     </main>
 </div>
 
