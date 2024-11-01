fb0ce40e0637fec33a6ae6d586f64fca3c91d308
ibrahim kedir <techofreact@gmail.com>
2024-11-01 11:44:07 +0300

    ft: promises section.

--- about_us_t.php
+++ about_us_t.php
@@ -15,5 +15,45 @@ get_header();
         <h1>About Us</h1>
         <p>Welcome to Tamesol, where collaboration, expertise, and client-centricity intersect to shape the future of digital innovation.</p>
     </div>
+    <!-- about tamesol -->
+</section>
+<section class="about-content-wrapper">
+    <article class="about_tamesol">
+        <h1 class="about_tam_title">About Tamesol</h1>
+        <p class="about_desc">Monarch Prime took root during an era of construction known for backbreaking work ethics and doing the right thing. That
+            mindset is woven into our business practices today.</p>
+    </article>
+    <article class="our_values_container">
+        <div class="image_container">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/values_picture.png" alt="our values" class="values_pic">
+        </div>
+        <div class="values_container_desc">
+            <h1 class="story_title">Our story</h1>
+            <p class="story_description">
+                The owner was first exposed to the industry through
+                weekend trips to job sites with her father, a successful
+                commercial general contractor. She joined his company at
+                14, performing secretarial duties and learning the nuances
+                of estimating multi-million dollar commercial projects.
+                Although she enjoyed her work at a thriving construction
+                company, she chose to pursue a career in pediatric
+                medicine. Still, her heart never left the trades. In 2009 she
+                re-entered the construction scene in D.C, dually seeing
+                patients and swinging a hammer for almost a decade.
+                Although she enjoyed her work at a thriving construction
+                company, she chose to pursue a career in pediatric
+                medicine. Still, her heart never left the trades. In 2009 she
+                re-entered the construction scene in D.C, dually seeing
+                patients and swinging a hammer for almost a decade.
+            </p>
+        </div>
+    </article>
+</section>
+<section class="about_promises_wrapper">
+    <div class="about-section">
+        <h1>Our promise</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit mus, imperdiet tempor faucibus nascetur vel et ante, non gravida praesent id nisi nunc maecenas. Magnis cum diam quisque etiam nostra ornare at, felis elementum lacinia commodo metus accumsan sed viverra, et facilisi luctus mi inceptos nisi. Placerat ligula taciti dignissim conubia donec rhoncus cursus vulputate, elementum habitant in sed etiam natoque duis nisi cubilia, nec dis netus tempor congue justo primis.</p>
+    </div>
+    <!-- about tamesol -->
 </section>
 <?php get_footer(); ?>
