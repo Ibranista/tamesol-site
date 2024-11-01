18ecdb9e93290b4defc2000852310b770f324f5e
ibrahim kedir <techofreact@gmail.com>
2024-11-01 14:10:18 +0300

    ft: about us section.

--- about_us_t.php
+++ about_us_t.php
@@ -54,6 +54,76 @@ get_header();
         <h1>Our promise</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit mus, imperdiet tempor faucibus nascetur vel et ante, non gravida praesent id nisi nunc maecenas. Magnis cum diam quisque etiam nostra ornare at, felis elementum lacinia commodo metus accumsan sed viverra, et facilisi luctus mi inceptos nisi. Placerat ligula taciti dignissim conubia donec rhoncus cursus vulputate, elementum habitant in sed etiam natoque duis nisi cubilia, nec dis netus tempor congue justo primis.</p>
     </div>
-    <!-- about tamesol -->
 </section>
+<section class="why-choose-us-section">
+    <h1 class="why_us_title">WHY CHOOSE US</h1>
+    <div class="why_us_container">
+        <p class="intro_desc">
+            There are hundreds of contractors you can choose to
+            complete your project. What makes us different? Why
+            should you choose us?
+        </p>
+        <section class="choose-us-box-wrapper">
+            <div class="experience-cards">
+                <div class="experience_card">
+                    <h1 class="exp_title">
+                        01 Comprehensive Media Solutions:
+                    </h1>
+                    <p class="exp_detail">
+                        With decades of experience,
+                        we can handle even the most
+                        challenging of projects.
+                    </p>
+                </div>
+                <div class="experience_card">
+                    <h1 class="exp_title">
+                        01 Comprehensive Media Solutions:
+                    </h1>
+                    <p class="exp_detail">
+                        With decades of experience,
+                        we can handle even the most
+                        challenging of projects.
+                    </p>
+                </div>
+                <div class="experience_card">
+                    <h1 class="exp_title">
+                        01 Comprehensive Media Solutions:
+                    </h1>
+                    <p class="exp_detail">
+                        With decades of experience,
+                        we can handle even the most
+                        challenging of projects.
+                    </p>
+                </div>
+                <div class="experience_card">
+                    <h1 class="exp_title">
+                        01 Comprehensive Media Solutions:
+                    </h1>
+                    <p class="exp_detail">
+                        With decades of experience,
+                        we can handle even the most
+                        challenging of projects.
+                    </p>
+                </div>
+            </div>
+            <div class="auditorium_img_wrapper">
+                <img src=" <?php echo get_template_directory_uri(); ?>/assets/images/why_choose_us_auditorium.jpeg" class="large_img_audit" alt="auditorium" />
+                <article class="camera_phone_img">
+                    <div class="img_cards_wrapper">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/camera_setup.jpeg" alt="camera_setup" />
+                    </div>
+                    <div class="img_cards_wrapper">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tech_globe.png" alt="globe_icon" />
+                    </div>
+                </article>
+            </div>
+            <button class="explore-btn">
+                <p>Explore Our work</p>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-arrow-right.svg" alt="right_arrow" class="explore-right-arrow" />
+            </button>
+        </section>
+    </div>
+</section>
+<?php get_template_part('template-parts/trusted-by'); ?>
+
 <?php get_footer(); ?>
--- front-page.php
+++ front-page.php
@@ -171,26 +171,8 @@ get_header(); ?>
                 </div>
             </div>
         </section>
-        <!-- trusted by section -->
-        <section class="trusted_by_section">
-            <div class="trusted_header_title">
-                <h2 class="trusted_by_title">Trusted By 50+ Companies</h2>
-            </div>
-            <div class="gained_trusts">
-                <div class="company_gained_wrapper">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/amhara_bank.png" alt="amhara_bank_icon" />
-                </div>
-                <div class="company_gained_wrapper">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/giz.png" alt="giz" />
-                </div>
-                <div class="company_gained_wrapper">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/telecom.png" alt="telecom" />
-                </div>
-                <div class="company_gained_wrapper">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cbe.png" alt="cbe" />
-                </div>
-            </div>
-        </section>
+        <!-- import trusted-by part from template parts the file is trusted-by.php -->
+        <?php get_template_part('template-parts/trusted-by'); ?>
     </main>
 </div>
 
